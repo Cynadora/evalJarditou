7b5cb3a4a6d2c69f71623e25c1d9a4f38aaf7943
Muzeaux Sylvie <sylvie.muzeaux@gmail.com>
2023-02-02 14:45:29 +0100

    evalexo5

--- Jarditou/js/script.js
+++ Jarditou/js/script.js
@@ -1,54 +1,102 @@
-// addEventListener() {
-
-    var erreur;
+    // var erreur;
     // récupération de tous les champs et un par un et les mettre dans une variable et récupérer nos champs avec document.getElementById
+    var valider = document.getElementById("envoyer");
     var nom = document.getElementById("nom");
     var prenom = document.getElementById("prenom");
     var datedenaissance = document.getElementById("datedenaissance");
-    var codepostal = document.getElementById("codepostal");
+    var code_postal = document.getElementById("code_postal");
     var adresse = document.getElementById("adresse");
     var ville = document.getElementById("ville");
     var email = document.getElementById("email");
     var sujet = document.getElementById("sujet");
-    var votrequestion = document.getElementById("votrequestion");
-// si if différent de nom (on détecte ici la valeur avec document.getElementById)
-    if(!nom.value) {
-        erreur = "Veuillez renseigner un nom";
-    }
-    if(!prenom.value) {
-        erreur = "Veuillez renseigner un prenom";
+    var textem=document.getElementById("textem");
+    var prenomm=document.getElementById("prenomm");
+    var codepostalm=document.getElementById("codepostalm");
+    var adressem=document.getElementById("adressem");
+    var villem=document.getElementById("villem");
+    var emailm=document.getElementById("emailm");
+    var datem=document.getElementById("datem");
+    var masculin=document.getElementById("masculin");
+    var feminin=document.getElementById("feminin");
+    var sexe_manquant=document.getElementById('sexe_manquant');
+    var accept_manquant=document.getElementById('accept_manquant');
+    var accepte=document.getElementById('accepte');
+    var question=document.getElementById('question');
+
+    var question_manquante=document.getElementById('question_manquante');
+    
+    
+
+    
+
+   
+    // Assigner à un événement plusieurs fonctions
+    valider.addEventListener("click", f_valid);
+
+    function f_valid(e){ 
         
-    }
-    if(!datedenaissance.value) {
-        erreur = "Veuillez renseigner un datedenaissance";
-    }
-    if(!codepostal.value) {
-        erreur = "Veuillez renseigner un codepostal";
-    }
-    if(!adresse.value) {
-        erreur = "Veuillez renseigner un adresse";
-       
-       
-        if(!ville.value) {
-            erreur = "Veuillez renseigner une ville";
+        if (nom.value === "") {   
+            e.preventDefault();
+            textem.innerHTML= 'nom manquant';
+            textem.style.color = "red";
         }
-        if(!email.value) {
-            erreur = "Veuillez renseigner un email";
+        
+
+
+        if (prenom.value === "") {
+            e.preventDefault();
+            prenomm.innerHTML= 'prenom manquant';
+            prenomm.style.color = "red";
         }
-        if(!sujet.value) {
-            erreur = "Veuillez renseigner un sujet";
+        if (code_postal.value === "") {
+            e.preventDefault();
+            codepostalm.innerHTML= 'code postal manquant';
+            codepostalm.style.color = "red";
         }
-        if(!votrequestion.value) {
-            erreur = "Veuillez renseigner votre question";
+        if (adresse.value === "") {
+            e.preventDefault();
+            adressem.innerHTML= 'Adresse manquante';
+            adressem.style.color = "red";
+        }
+        if (ville.value === "") {
+            e.preventDefault();
+            villem.innerHTML= 'ville manquante';
+            villem.style.color = "red";
+        }
+        if (email.value === "") {
+            e.preventDefault();
+            emailm.innerHTML= 'email manquant';
+            emailm.style.color = "red";
+        }
+        if (date.value === "") {
+            e.preventDefault();
+            datem.innerHTML= 'date de naissance manquante';
+            datem.style.color = "red";
+        }                   
+            if (masculin.checked || feminin.checked)  {
+            sexe_manquant.style.display='none';
+            }
+            else{
+                e.preventDefault();    
+                sexe_manquant.innerHTML="sexe manquant";
+                sexe_manquant.style.color = "red";
+            }
+
+            if (accepte.checked )  {
+                accept_manquant.style.display='none';
+                }
+                else{
+                    e.preventDefault();    
+                    accept_manquant.innerHTML="Veuillez cochez cette case";
+                    accept_manquant.style.color = "red";    
+                }
+
+                if (question.value === "") {   
+                    e.preventDefault();
+                    question_manquante.innerHTML= 'question manquante';
+                    question_manquante.style.color = "red";
+                }
+
+
     }
-        if(erreur) {
-            document.getElementById("erreur").innerHTML = erreur;
-        }
-
-
-
-
-
-
-    alert("Formulaire envoyé !");
-}
+f_valid(e);
